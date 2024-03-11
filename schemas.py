d14f45f5da92f882df1b93a09418a029be5361e8
Danny Gleason <danny.c.gleason@gmail.com>
2024-03-11 15:48:48 -0400

    update

--- schemas.py
+++ schemas.py
@@ -17,31 +17,32 @@ class LinkedinConfig(BaseModel):
     remote: List[str]
     salary: List[str]
     sort: List[str]
-    blacklistCompanies: List[str] = []
-    blockCompanies: List[str] = []  # Add the missing field
-    blackListTitles: List[str] = []
-    followCompanies: bool
-    preferredCv: int
-    outputSkippedQuestions: bool
-    useAiAutocomplete: bool
-    onlyApplyCompanies: List[str] = []
-    onlyApplyTitles: List[str] = []
-    blockHiringMember: List[str] = []
-    onlyApplyHiringMember: List[str] = []
-    onlyApplyMaxApplications: List[str] = []
-    onlyApplyMinApplications: List[str] = []
-    onlyApplyJobDescription: List[str] = []
-    blockJobDescription: List[str] = []
-    onlyApplyMimEmployee: List[str] = []
-    onlyApplyLinkedinRecommending: bool
-    onlyApplySkilledBadges: bool  # Rename the field
-    saveBeforeApply: bool
-    messageToHiringManager: Optional[str] = None
-    listNonEasyApplyJobsUrl: bool
-    defaultRadioOption: int
-    answerAllCheckboxes: Optional[bool] = None
-    outputFileType: List[str]
     chromeHeadless: bool  # Add the missing field
+
+    # blacklistCompanies: List[str] = []
+    # blockCompanies: List[str] = []  # Add the missing field
+    # blackListTitles: List[str] = []
+    # followCompanies: bool
+    # preferredCv: int
+    # outputSkippedQuestions: bool
+    # useAiAutocomplete: bool
+    # onlyApplyCompanies: List[str] = []
+    # onlyApplyTitles: List[str] = []
+    # blockHiringMember: List[str] = []
+    # onlyApplyHiringMember: List[str] = []
+    # onlyApplyMaxApplications: List[str] = []
+    # onlyApplyMinApplications: List[str] = []
+    # onlyApplyJobDescription: List[str] = []
+    # blockJobDescription: List[str] = []
+    # onlyApplyMimEmployee: List[str] = []
+    # onlyApplyLinkedinRecommending: bool
+    # onlyApplySkilledBadges: bool  # Rename the field
+    # saveBeforeApply: bool
+    # messageToHiringManager: Optional[str] = None
+    # listNonEasyApplyJobsUrl: bool
+    # defaultRadioOption: int
+    # answerAllCheckboxes: Optional[bool] = None
+    # outputFileType: List[str]
 
 class ApplyDetails(BaseModel):
     email: EmailStr
