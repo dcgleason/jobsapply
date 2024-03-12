be0953ead9252ba5c5b2caef52c9628da20dfda9
Danny Gleason <danny.c.gleason@gmail.com>
2024-03-12 15:33:16 -0400

    update selenium

--- linkedin.py
+++ linkedin.py
@@ -22,7 +22,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -30,7 +29,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import Select
-
 
 
 
