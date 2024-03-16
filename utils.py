fbc989ab3e08a2466c2cecb158b1d9c97d16cbac
Danny Gleason <danny.c.gleason@gmail.com>
2024-03-16 17:28:56 -0400

    update

--- utils.py
+++ utils.py
@@ -15,6 +15,7 @@ def chromeBrowserOptions(config: LinkedinConfig):
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument("--window-size=1920,1080")
 
+
     if(config.chromeHeadless):
         options.add_argument("--headless")
     options.add_argument("--start-maximized")
