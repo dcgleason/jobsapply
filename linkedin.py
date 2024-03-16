28cce52fbae1d204215ad0ec8e9120e42e4331d6
Danny Gleason <danny.c.gleason@gmail.com>
2024-03-16 18:31:29 -0400

    udpate procfile

--- linkedin.py
+++ linkedin.py
@@ -452,9 +452,9 @@ class Linkedin:
 
                 for attempt in range(max_retries):
                     try:
-                        WebDriverWait(self.driver, 60 + attempt * retry_delay).until(
-                            lambda driver: driver.execute_script("return document.readyState") == "complete"
-                        )
+                        # WebDriverWait(self.driver, 60 + attempt * retry_delay).until(
+                        #     lambda driver: driver.execute_script("return document.readyState") == "complete"
+                        # )
                         WebDriverWait(self.driver, 60 + attempt * retry_delay).until(
                             EC.presence_of_element_located((By.XPATH, "//div[@class='jobs-search-results-list__title-heading']/small/div/span"))
                         )
